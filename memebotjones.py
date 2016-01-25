b9bc8381518fff5b8a679de0e795ccd0d49204ea
actioninja <actioninja@gmail.com>
2016-01-24 21:52:25 -0800

    [ADD]: Large Additions, see full commit for details

--- memebotjones.py
+++ memebotjones.py
@@ -1,17 +1,27 @@
-import sys, discord, json,  asyncio
+import sys
+import discord
+import json
+import asyncio
+import traceback
+import memebotjones
 
 
+# Get the config and load it as the variable config
+# Also catch if there isn't one there, and generate a new one.
 try:
     with open('config.json') as data:
         config = json.load(data)
 except FileNotFoundError:
     print("You do not have a config file!  New one generated as config.json, exiting.")
     with open('config.json', 'w') as outfile:
-        data = {'version': "0.1", 'email': "", 'password': "", 'ownerid': "", 'invoker': "$", 'voicechannel': ""}
+        data = {'version': "0.1", 'email': "", 'password': "", 'ownerid': "", 'invoker': "$", 'voicechannel': "", 'numberofvotestoskip': 4}
         json.dump(data, outfile)
         sys.exit()
 
 
+# If the config is outdated, some nasty ass shit can happen.
+# Update this variable only when the config gets new parameters.
+# TODO: Make it auto add the new required variables.
 if config['version'] != "0.1":
     print("Your config file is outdated.  Please update your config with new values added since last patch.")
     sys.exit()
@@ -22,19 +32,7 @@ client = discord.Client(max_messages=3000)
 voice = None
 
 
-def move_queue():
-    linkspool.pop(0)
-    if linkspool[0] != "":
-        play_next()
 
-
-def play_next():
-    try:
-        player = voice.create_ytdl_player(linkspool[0], after=move_queue())
-        player.start()
-    except discord.ClientException:
-        print('invalid link, skipping..')
-        play_next()
 
 
 @client.async_event
@@ -43,26 +41,34 @@ def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("-------------------------------")
-    channel = discord.utils.get(client.get_all_channels(), name=config['voicechannel'])
-    global voice
-    voice = yield from client.join_voice_channel(channel)
+    # Using the voice channel value from the config, the bot will immediately connect to the voice channel as soon as it
+    # is running.
+
 
 
 @client.async_event
 def on_message(message):
-    if message.content.startswith(config['invoker']):
-        print(message.content[1:].split(' ')[0])
-        linkspool.append(message.content[1:].split(' ')[0])
-        print(linkspool)
-        if len(linkspool) == 1:
-
-            play_next()
+    if message.content.startswith(config['invoker']) and message.author.id != client.user.id:
+        command = message.content[1:].split(' ')[0].lower()
+        if command in memebotjones.base.functions:
+            yield from client.send_typing(message.channel)
+            yield from memebotjones.base.functions[command](message, client)
 
 
 @client.async_event
 def on_typing(channel, user, when):
+    # :^)
+    # Bonus points if you figure out who's id this is.
     if user.id == "81216735872548864":
         print("The goy is talking")
+
+
+@client.async_event
+def on_error(event):
+    print("Caught error, dumping queue")
+    print(traceback.format_exc())
+    memebotjones.music.clear_queue()
+
 
 
 def main_task():
@@ -74,6 +80,7 @@ def main_task():
         sys.exit()
 
 
+# I don't really know what this does, but it's necessary
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main_task())
 loop.close()
---
+++ memebotjones/__init__.py
@@ -0,0 +1,5 @@
+# Import all of the modules as their base names instead of having to refer to them as memebot.*
+# apparently this is a bad practice or something but I'm going to do it too.
+
+import memebotjones.base as base
+import memebotjones.help as memebothelp
